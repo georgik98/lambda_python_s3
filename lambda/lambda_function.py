dd06e0c4fa09e5069eb86f65a8ee2e6c72b798eb
Georgi Kolev <georgipetrovkolev1998@gmail.com>
2024-04-15 14:06:05 +0300

    lambda-notification-sns-python-func-works

---
+++ lambda/lambda_function.py
@@ -0,0 +1,40 @@
+import json
+import boto3 # type: ignore
+
+def lambda_handler(event, context):
+    # Initialize the SNS client
+    sns = boto3.client('sns')
+    
+    # Specify the ARN of your SNS topic
+    topic_arn = 'arn:aws:sns:eu-central-1:360980374647:my-notification-topic'
+    
+    # Process each record from the S3 bucket event
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        object_key = record['s3']['object']['key']
+        size = record['s3']['object'].get('size', 0)  # Get size, default to 0 if not present
+        
+        # Create a message to send via SNS
+        message = {
+            'message': 'A new file has been uploaded.',
+            'file_name': object_key,
+            'bucket_name': bucket_name,
+            'size': size
+        }
+        
+        # Convert message dictionary to JSON format
+        message_string = json.dumps(message)
+        
+        # Publish message to SNS
+        response = sns.publish(
+            TopicArn=topic_arn,
+            Message=message_string,
+            Subject='New File Uploaded to S3 Bucket'
+        )
+        
+        print("SNS publish response:", response)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully processed S3 event.')
+    }
---
+++ main.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_sns_topic" "my_topic" {
+  name = "my-notification-topic"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.my_topic.arn
+  protocol  = "email"
+  endpoint  = "georgipetrovkolev1998@gmail.com"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-s3-bucket-georgik16-123123-1231"
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/lambda_function.py"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_s3_bucket_object" "lambda_code" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "lambda_function.zip"
+  source = data.archive_file.lambda.output_path
+  etag   = filemd5(data.archive_file.lambda.output_path)
+}
+
+resource "aws_lambda_function" "s3_notification_lambda" {
+  function_name = "S3NotificationLambda"
+  s3_bucket     = aws_s3_bucket.my_bucket.bucket
+  s3_key        = "lambda_function.zip"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  role          = aws_iam_role.lambda_exec_role.arn
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+  depends_on       = [aws_s3_bucket_object.lambda_code]
+}
+
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_notification_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.my_bucket.arn
+}
+
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_policy"
+  description = "IAM policy for Lambda to access S3 and SNS"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "sns:Publish",
+          "s3:GetObject"
+        ],
+        Resource = "*",
+        Effect   = "Allow",
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+
+resource "aws_lambda_permission" "allow_S3_invoke" {
+  statement_id  = "AllowExecutionFromS3BucketUnique"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_notification_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.my_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3_notification_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
